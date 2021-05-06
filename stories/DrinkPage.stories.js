6c21675f1e5bd9c2556812b3ddc068a6f9028bc4
John <Johnnyk737@gmail.com>
2021-05-06 23:00:25 +0100

    Adding drinkpage storybook

---
+++ stories/DrinkPage.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { DrinkPage } from '..app/components/drinkpage/DrinkPage.js';
+import StoryRouter from 'storybook-react-router';
+
+export default {
+  title: 'Main/DrinkPage',
+  component: DrinkPage,
+  decorators: [StoryRouter()]
+};
+
+const Template = (args) => <DrinkPage {...args} />;
+export const Default = Template.bind({});
+Default.args = {
+  drink: {
+    "id": 1,
+    "drinkName": "Old Fashioned",
+    "imageUrl": "https://cdn.liquor.com/wp-content/uploads/2018/05/08113350/bourbon-old-fashioned-720x720-recipe.jpg",
+    "ingredientList": ["1.5 fl oz Bourbon Whiskey", 
+                      "2 tsp simple syrup", 
+                      "2 dashes Angostura bitters", 
+                      "1 orange slice",
+                      "1 Marachino cherry (Optional)", 
+                      "Ice cubes"],
+    "baseIngredients": ["bourbon whiskey", "simple syrup", "angostura bitters", "orange", "cherry"],
+    "directions": ["Pour the simple syrup, water, and bitters into a mixing glass. Stir to combine",
+                  "Place the ice cubes in the glass.",
+                  "Pour bourbon over the ice.",
+                  "Garnish with the orange slice and maraschino cherry."],
+    "tags": "",
+    "glass": "",
+    "alchohol": ["whiskey", "bourbon"],
+    "flavors": ["sweet", "bitter"]
+  }
+};
